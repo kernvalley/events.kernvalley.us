66f553a6cca8d7223a32e990b215bf06b28f0998
Chris Zuber <shgysk8zer0@gmail.com>
2019-11-29 13:20:06 -0800

    WIP updating design
    This will take quite a bit of work...

--- js/index.js
+++ js/index.js
@@ -2,10 +2,13 @@ import 'https://cdn.kernvalley.us/js/std-js/shims.js';
 import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/components/share-button.js';
 import {ready, $, registerServiceWorker} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import HTMLOpenStreetMapElement from 'https://cdn.kernvalley.us/components/open-street-map.js';
 
 document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
 document.documentElement.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
 document.documentElement.classList.replace('no-js', 'js');
+
+customElements.define(HTMLOpenStreetMapElement.tagName, HTMLOpenStreetMapElement);
 
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
 	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
