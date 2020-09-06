5cc9156d62c416039a6755ee2d702cafc500e934
Chris Zuber <shgysk8zer0@gmail.com>
2020-09-06 12:20:05 -0700

    Various updates
    ### Added
    - Track external link clicks with GA
    
    ### Changed
    - Make properties for preloading, etc. consistent
    - Update components/`_includes/`
    - Use `"no-referrer"` as default referrer policy
    
    ### Fixed
    - Disable bash script linting for issue with `.rvmrc`

--- js/index.js
+++ js/index.js
@@ -18,8 +18,37 @@ document.documentElement.classList.toggle('no-dialog', document.createElement('d
 document.documentElement.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
 document.documentElement.classList.replace('no-js', 'js');
 
-if (typeof GA === 'string' && GA !== '') {
-	importGa(GA);
+if (typeof GA === 'string' && GA.length !== 0) {
+	importGa(GA).then(async () => {
+		/* global ga */
+		ga('create', GA, 'auto');
+		ga('set', 'transport', 'beacon');
+		ga('send', 'pageview');
+
+		function outbound() {
+			ga('send', {
+				hitType: 'event',
+				eventCategory: 'outbound',
+				eventAction: 'click',
+				eventLabel: this.href,
+				transport: 'beacon',
+			});
+		}
+
+		function madeCall() {
+			ga('send', {
+				hitType: 'event',
+				eventCategory: 'call',
+				eventLabel: 'Called',
+				transport: 'beacon',
+			});
+		}
+
+		await ready();
+
+		$('a[rel~="external"]').click(outbound, { passive: true, capture: true });
+		$('a[href^="tel:"]').click(madeCall, { passive: true, capture: true });
+	});
 }
 
 Promise.all([
