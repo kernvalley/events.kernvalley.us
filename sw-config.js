8636381282b8e0dd8fb1ee57b7ea99c7ed3ff2ba
Chris Zuber <shgysk8zer0@gmail.com>
2021-01-27 11:31:32 -0800

    Remove unused components
    Also updates SW cache config

--- sw-config.js
+++ sw-config.js
@@ -2,7 +2,9 @@
 layout: null
 ---
 'use strict';
-/*eslint no-unused-vars: 0*/
+/* eslint-env serviceworker */
+/* eslint no-unused-vars: 0 */
+
 const config = {
 	version: '{{ site.data.app.version | default: site.version }}',
 	fresh: [
@@ -12,24 +14,22 @@ const config = {
 		'/manifest.json',
 	].map(path => new URL(path, location.origin).href),
 	stale: [
-		'/css/index.min.css',
-		'/js/index.min.js',
-		'/img/icons.svg',
-
 		/* JS, `customElements`, etc. */
+		'/js/index.min.js',
 		'https://cdn.kernvalley.us/components/toast-message.html',
 		'https://cdn.kernvalley.us/components/share-to-button/share-to-button.html',
 		'https://cdn.kernvalley.us/components/pwa/prompt.html',
 		'https://cdn.kernvalley.us/components/weather-current.html',
 
 		/* CSS */
-		'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+		'/css/index.min.css',
 		'https://cdn.kernvalley.us/components/toast-message.css',
 		'https://cdn.kernvalley.us/components/share-to-button/share-to-button.css',
 		'https://cdn.kernvalley.us/components/pwa/prompt.css',
 		'https://cdn.kernvalley.us/components/weather-current.css',
 
 		/* Images & Icons */
+		'/img/icons.svg',
 		'/img/apple-touch-icon.png',
 		'/img/icon-192.png',
 		'/img/favicon.svg',
@@ -41,15 +41,14 @@ const config = {
 
 		/* Fonts */
 		'https://cdn.kernvalley.us/fonts/roboto.woff2',
-		'https://cdn.kernvalley.us/fonts/Libertine.woff',
-		'https://cdn.kernvalley.us/fonts/ubuntu.woff2',
-		/* Other */
 	].map(path => new URL(path, location.origin).href),
 	allowed: [
-		/https:\/\/i\.imgur\.com\/*/,
-		/https:\/\/maps\.wikimedia\.org\/osm-intl\/*/,
-		/https:\/\/events\.kernvalley\.us\/events\/*/,
-		/https:\/\/api\.github\.com\/users\/*/,
-		/https:\/\/*\.githubusercontent\.com\/u\/*/,
+		'https://i.imgur.com/',
+		'https://cdn.kernvalley.us/img/branding/',
+		/https:\/\/\w+\.githubusercontent\.com\/u\/*/,
 	],
+	allowedFresh: [
+		'https://api.github.com/users/',
+		'https://api.openweathermap.org/data/2.5/weather',
+	]
 };
