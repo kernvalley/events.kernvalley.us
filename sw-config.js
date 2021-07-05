d2286c15717741b898bf89da34da70fffb642738
Chris Zuber <shgysk8zer0@gmail.com>
2021-07-05 13:06:02 -0700

    More misc. layout updates

--- sw-config.js
+++ sw-config.js
@@ -46,7 +46,7 @@ const config = {
 		'https://i.imgur.com/',
 		'https://cdn.kernvalley.us/img/branding/',
 		/https:\/\/\w+\.githubusercontent\.com\/u\/*/,
-		/\.(jpg|png|gif|webp|ico)$/,
+		/\.(jpg|png|gif|webp|ico|woff2)$/,
 	],
 	allowedFresh: [
 		'https://api.github.com/users/',
