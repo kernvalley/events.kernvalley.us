c2ef7c5a3123ae53ecf64ee67ea2642f030d82ec
Chris Zuber <shgysk8zer0@gmail.com>
2021-07-05 13:17:51 -0700

    More layout fixes

--- sw-config.js
+++ sw-config.js
@@ -46,7 +46,7 @@ const config = {
 		'https://i.imgur.com/',
 		'https://cdn.kernvalley.us/img/branding/',
 		/https:\/\/\w+\.githubusercontent\.com\/u\/*/,
-		/\.(jpg|png|gif|webp|ico|woff2)$/,
+		/\.(jpg|png|gif|webp|ico|woff2|woff)$/,
 	],
 	allowedFresh: [
 		'https://api.github.com/users/',
